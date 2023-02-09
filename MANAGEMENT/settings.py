b74917e6f886f316bf08d2bc75d3d80a2f09c074
ErtelDG <ertel.developer@t-online.de>
2023-02-09 10:22:23 +0100

    resetPasswordMethod

--- MANAGEMENT/settings.py
+++ MANAGEMENT/settings.py
@@ -149,3 +149,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/management/"
 LOGOUT_REDIRECT_URL = "/home"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"  
