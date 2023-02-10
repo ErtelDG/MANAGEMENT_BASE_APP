eca0e7e9bd14aa21ae1517c3db053f451dc8abbc
ErtelDG <ertel.developer@t-online.de>
2023-02-10 13:04:46 +0100

    cnewTaskToDB

--- apps/kanban_board/views.py
+++ apps/kanban_board/views.py
@@ -1,18 +1,40 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 from django.template import loader
-
+from datetime import datetime
 from kanban_board.models import Task
 
 def index_kanban_board(request):
+    
+   
+    
     if request.method == 'POST':
-        my_database = Task.objects.all()
+        userList = User.objects.all()
+             
+        print(request.POST.get("newTaskTitle"))        
+        print(request.POST.get("newTaskDescription"))        
+        print(request.POST.get("newTaskPrio"))        
+        print(request.POST.get("newMemberType"))        
+        print(request.POST.get("newTaskAssigned"))        
+        print(request.POST.get("newTaskStatus"))
+        Task.objects.create(
+            title = request.POST["newTaskTitle"],
+            description=request.POST["newTaskDescription"],
+            prio = request.POST["newTaskPrio"],
+            member_type=request.POST["newMemberType"],
+            assigned=User.objects.get(pk=request.POST["newTaskAssigned"]),
+            status=request.POST["newTaskStatus"],
+            created_at = datetime.now(),
+            updated_at = datetime.now(),
+        )
+                  
         your_variable =['Hallo Noob','Hallo Noob2','Hallo Noob3']   
-        return render(request, 'kanban_board/kanban_board_template.html',{'WERT1':your_variable, 'DATABASE':my_database, 'responseInfoAddTaskCreated': 'New task successfully',})
+        return render(request, 'kanban_board/kanban_board_template.html',{'WERT1':your_variable, 'responseInfoAddTaskCreated': 'New task successfully', 'users': userList})
     
     if request.method == 'GET':
-        return render(request, 'kanban_board/kanban_board_template.html')
-    return render(request, 'kanban_board/kanban_board_template.html',{'WERT1':'NO VALUE REQUESTT', 'DATABASE':'NO VALUE REQUEST DATABASE'})
+        userList = User.objects.all()
+        return render(request, 'kanban_board/kanban_board_template.html', {'users': userList})
+    return render(request, 'kanban_board/kanban_board_template.html',{'WERT1':'NO VALUE REQUEST'})
 
 
 
