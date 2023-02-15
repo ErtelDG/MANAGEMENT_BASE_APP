139cf63892931c32b65c583a45454bed384aef1f
ErtelDG <ertel.developer@t-online.de>
2023-02-15 12:56:36 +0100

    signUpUserCkeckAndValidation

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,7 @@
 # Importing the render function from the django.shortcuts module.
 from django.shortcuts import render
 from django.contrib.auth.models import User
+import re
 
 
 """
@@ -22,12 +23,35 @@ It creates a user object with the information provided by the user in the regist
 def registered(request):
     
     if request.method == "POST":
-       
-        user = User.objects.create_user(
-            username=request.POST.get("username"),
-            first_name=request.POST.get("firstname"),
-            last_name=request.POST.get("lastname"),
-            email=request.POST.get("useremail"),
-            password=request.POST.get("userpassword"),
-        )
+        getNewUsername = request.POST.get("username")
+        getFirstName=request.POST.get("firstname")
+        getLastName=request.POST.get("lastname")
+        getEmail=request.POST.get("useremail")
+        getPassword=request.POST.get("userpassword")
+        
+        lengthNewUsername = len(getNewUsername)
+        lengthFirstName = len(getFirstName)
+        lengthLastName = len(getLastName)
+        lengthPassword = len(getPassword)
+        
+        if User.objects.filter(username=getNewUsername).exists():
+            return render(request, 'registration/signup.html',{'mistakeUsernameExists': 'User exists. Try another username.', 'inputEmail':getEmail,'inputLastName':getLastName,'inputFirstName':getFirstName, 'inputUsername':getNewUsername, 'inputPassword':getPassword})
+        elif  lengthNewUsername < 5 or lengthNewUsername > 20:
+            return render(request, 'registration/signup.html',{'mistakeUsername': 'Required. 5-20 characters or fewer. Letters only.', 'inputEmail':getEmail,'inputLastName':getLastName,'inputFirstName':getFirstName, 'inputUsername':getNewUsername, 'inputPassword':getPassword})
+        elif lengthFirstName < 2 or lengthFirstName > 20:
+            return render(request, 'registration/signup.html',{'mistakeFirstName': 'Required. 2-20 characters or fewer. Letters only.','inputEmail':getEmail,'inputLastName':getLastName,'inputFirstName':getFirstName, 'inputUsername':getNewUsername, 'inputPassword':getPassword})
+        elif lengthLastName < 2 or lengthLastName > 20:
+            return render(request, 'registration/signup.html',{'mistakeLastName': 'Required. 2-20 characters or fewer. Letters only.','inputEmail':getEmail,'inputLastName':getLastName,'inputFirstName':getFirstName, 'inputUsername':getNewUsername, 'inputPassword':getPassword})
+        elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", getEmail):
+            return render(request, 'registration/signup.html',{'mistakeEmail': 'Enter a valid email!','inputEmail':getEmail,'inputLastName':getLastName,'inputFirstName':getFirstName, 'inputUsername':getNewUsername, 'inputPassword':getPassword})
+        elif lengthPassword < 8:
+            return render(request, 'registration/signup.html',{'mistakePassword': 'Your password must contain at least 8 characters.','inputEmail':getEmail,'inputLastName':getLastName,'inputFirstName':getFirstName, 'inputUsername':getNewUsername, 'inputPassword':getPassword})
+        else:
+            user = User.objects.create_user(
+                username=getNewUsername,
+                first_name=getFirstName,
+                last_name=getLastName,
+                email=getEmail,
+                password=getPassword,
+            )
     return render(request, 'registration/registered.html',)
--- apps/kanban_board/models.py
+++ apps/kanban_board/models.py
@@ -31,6 +31,11 @@ STATUS_CHOICES=[
   ('Done','Done'),
 ]
 
+class Subtask(models.Model):
+    title = models.CharField(max_length=50, default="No Title")
+    status_subtask = models.BooleanField("Status", default=False)
+
+
 
 # It creates a model for the database.
 class Task(models.Model):
@@ -42,6 +47,7 @@ class Task(models.Model):
     status=models.CharField(max_length=20, choices=STATUS_CHOICES, default='ToDo')
     created_at = models.DateTimeField('date created')
     updated_at = models.DateTimeField('date updated')
+    sub_task = models.CharField(max_length=10, default="No subtask")
     
     #It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.
     #def __str__(self):
