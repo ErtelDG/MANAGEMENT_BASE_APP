c8ba088e29f21ee7a31a7a9b050e156d9997528d
ErtelDG <ertel.developer@t-online.de>
2023-02-18 14:54:19 +0100

    createModelsSubtasks

--- apps/kanban_board/admin.py
+++ apps/kanban_board/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # Import models for show on the admin side
-from .models import Task
+from .models import Task, Subtask
 
 # Register imported models:
 admin.site.register(Task)
+admin.site.register(Subtask)
--- apps/kanban_board/models.py
+++ apps/kanban_board/models.py
@@ -31,29 +31,30 @@ STATUS_CHOICES=[
   ('Done','Done'),
 ]
 
-class Subtask(models.Model):
-    title = models.CharField(max_length=50, default="No Title")
-    status_subtask = models.BooleanField("Status", default=False)
-
-
 
 # It creates a model for the database.
 class Task(models.Model):
-    title = models.CharField(max_length=200, default="No Title")
-    description = models.CharField(max_length=400, default="No description")
-    prio = models.CharField(max_length=20, choices=PRIO_CHOICES, default='Low')
-    member_type = models.CharField(max_length=20, choices=MEMBER_TYPE_CHOICES, default='Frontend')
-    assigned = models.ForeignKey(User, on_delete=models.PROTECT)
-    status=models.CharField(max_length=20, choices=STATUS_CHOICES, default='ToDo')
-    created_at = models.DateTimeField('date created')
-    updated_at = models.DateTimeField('date updated')
-    sub_task = models.CharField(max_length=10, default="No subtask")
-    newCol  = models.CharField(max_length=200, default="No Title")
-    newCol2  = models.CharField(max_length=200, default="No Title")
+  title = models.CharField(max_length=200, default="No Title")
+  description = models.CharField(max_length=400, default="No description")
+  prio = models.CharField(max_length=20, choices=PRIO_CHOICES, default='Low')
+  member_type = models.CharField(max_length=20, choices=MEMBER_TYPE_CHOICES, default='Frontend')
+  assigned = models.ForeignKey(User, on_delete=models.PROTECT)
+  status=models.CharField(max_length=20, choices=STATUS_CHOICES, default='ToDo')
+  created_at = models.DateTimeField('date created')
+  updated_at = models.DateTimeField('date updated')
+  
+  def __str__(self):
+      return self.title
     
-    #It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.
-    #def __str__(self):
-    #    return self.choice_text
+  #It’s important to add __str__() methods to your models, not only for your own convenience when dealing with the interactive prompt, but also because objects’ representations are used throughout Django’s automatically-generated admin.
+  #def __str__(self):
+  #    return self.choice_text
     
-    def was_published_recently(self):
+  def was_published_recently(self):
         return (timezone.now() - self.created_at).days
+  
+  
+class Subtask(models.Model):
+  task = models.ForeignKey(Task, on_delete=models.CASCADE)
+  title = models.CharField(max_length=50, default="No Title")
+  status_subtask = models.BooleanField("Status", default=False)
